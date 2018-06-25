47c281bbad95ee5d7bbc027bcd41a8ab6dd42cbf
Gekios <ggekios@gmail.com>
2018-06-25 14:59:46 +0200

    Changed verify-scripts-json.py according to nmav's feedback and edited yml in order to work

--- verify-scripts-json.py
+++ verify-scripts-json.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python
 
 import os
-from os import listdir
+from os import listdir, path
 import json
 from os.path import isfile, join
-
+from sys import argv
 
 dir = os.path.dirname(__file__)
 scriptlist = [f for f in listdir(os.path.join(dir,'scripts')) if isfile(join(os.path.join(dir,'scripts'), f))]
 missing = []
-missingrns = []
-tlslite = json.load(open(os.path.join(dir,'tests/tlslite-ng.json')))
-tlslitern = json.load(open(os.path.join(dir,'tests/tlslite-ng-random-subset.json')))
-for f in scriptlist:
-	if f not in str(tlslite):
-		missing.append(f)
-	if f not in str(tlslitern):
-		missingrns.append(f)
 
-if not missing and not missingrns:
-	print(" All scripts are int the json files")
-elif set(missing) == set(missingrns):
-	print("There are " + str(len(missing)) + " scripts that are missing on both tlslite-ng.json and tlslite-ng.json :")
-	print("\n".join(missing))
+try:
+	jsonfile = json.load(open(os.path.join(dir,argv[1])))
+except IOError:
+	print("Please check the input file name, it doesn't appear to exist")
+	exit(1)
+except ValueError:
+	print("Wrong file type: The input must be a json file")
+	exit(1)
+except IndexError:
+	print("No input file was provided")
+	exit(1)
+
+for f in scriptlist:
+	if f not in str(jsonfile):
+		missing.append(f)
+
+if not missing:
+	print(" All scripts are in the json file")
+	exit(0)
 else:
-	print("There are " + str(len(missing)) + " scripts that are missing on tlslite-ng.json : ")
+	print("There are " + str(len(missing)) + " scripts that are missing from the file:")
 	print("\n".join(missing))
-	print("There are " + str(len(missingrns)) + " scripts that are missing on tlslite-ng.json : ".format(len(missingrns)))
-	print("\n".join(missingrns))
+	exit(1)
